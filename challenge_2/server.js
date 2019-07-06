ae53b51aeee7e00a9e9b31b109240e322681310c
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-07-06 12:15:32 -0700

    Set up basic html file and express server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(port, console.log(`Listening on port ${port}`));
 
