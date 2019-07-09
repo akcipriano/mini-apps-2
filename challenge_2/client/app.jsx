f818bd00188d027fad108c15a9f623aa58149344
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-07-08 22:51:42 -0700

    Complete Basic Requirements

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,5 +1,85 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Chart from 'chart.js';
 
-ReactDOM.render(<div> REACT</div>, document.getElementById('app'));
+class HistoryChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dates: [],
+      values: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/historicaldata')
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log('data:', JSON.stringify(data));
+      const returnedDates = [];
+      const returnedValues = [];
+
+      for (var key in data.bpi) {
+        returnedDates.push(key);
+        returnedValues.push(data.bpi[key]);
+      }
+
+      this.setState({
+        dates: returnedDates,
+        values: returnedValues
+      },
+        () => {
+          const myChart = new Chart(document.getElementById('myChart'), {
+            type: 'line',
+            data: {
+              labels: this.state.dates,
+              datasets: [{
+                label: 'Closing Price (USD $)',
+                data: this.state.values,
+                fill: false,
+                borderColor: 'rgba(248, 173, 22, 1)',
+              }]
+            },
+            options: {
+              title: {
+                display: true,
+                text: 'Bitcoin Price Index',
+                fontSize: 15
+              },
+              elements: {
+                line: {
+                  tension: 0
+                }
+              },
+              scales: {
+                xAxes: [{
+                  type: 'time',
+                  time: {
+                    displayFormats: {
+                      quarter: 'll'
+                    }
+                  }
+                }]
+              }
+            }
+          });
+        }
+      );
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <canvas id="myChart" width="150" height="50"></canvas>
+        <br />
+        Powered by <a href="https://www.coindesk.com/price/bitcoin" target="_blank">Coin Desk</a>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<HistoryChart />, document.getElementById('app'));
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const request = require('request');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,6 +9,13 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/historicaldata', (req, res) => {
+  request('https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-06-01&end=2019-06-30', (error, response, body) =>{
+    if (error) {console.error('BPI error:', error);}
+    res.status(200).send(body);
+  })
+});
 
 app.listen(port, console.log(`Listening on port ${port}`));
 
