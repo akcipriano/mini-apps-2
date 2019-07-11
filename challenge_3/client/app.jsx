dfd99481cc4b10322b0770dd3af385d951da582e
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-07-10 19:36:34 -0700

    Set up Express server, React/Webpack and HTML

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class Bowling extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //state: something
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        React
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Bowling />, document.getElementById('app'));
 
---
+++ challenge_3/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/app.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/public')
+  }
+}
